4e2fe1e9c764b05a4b1fbdb05d0826c70b5a1ea4
jumoney <jumoney15@naver.com>
2020-10-07 10:53:30 +0900

    2222

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,23 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		int n1 = 10;
+		int n2 = 20;
+		assertEquals(n1 + n2, Calc.sum(n1, n2));
+		
+		n1 = 5;
+		n2 = 5;
+		assertEquals(n1 + n2, Calc.sum(n1, n2));
+
+	}
+
+}
